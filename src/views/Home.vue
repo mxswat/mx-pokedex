2ca325e04841f6b26d296a5da1bb84b1ac95f38b
Massimo Mx Cacciapaglia <massimocacciapaglia.13@gmail.com>
2020-08-25 12:04:49 +0200

    Test stuff

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,22 @@
   <div class="home">
     <h1>Welcome to MxPokedex!</h1>
     <h3>Click on any pokemon or use the search</h3>
+    <!-- Apollo watched Graphql query -->
+    <ApolloQuery :query="require('../graphql/PokemonHome.gql')" :variables="{ limit }">
+      <template slot-scope="{ result: { loading, error, data } }">
+        <!-- Loading -->
+        <div v-if="loading" class="loading apollo">Loading...</div>
+
+        <!-- Error -->
+        <div v-else-if="error" class="error apollo">An error occured</div>
+
+        <!-- Result -->
+        <div v-else-if="data" class="result apollo">{{ data.pokemons.results.length }}</div>
+
+        <!-- No result -->
+        <div v-else class="no-result apollo">No result :(</div>
+      </template>
+    </ApolloQuery>
   </div>
 </template>
 
@@ -10,9 +26,14 @@
 // import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     // HelloWorld
+  },
+  data () {
+    return {
+      limit: 1050, //1048 is the number of pokemon now!
+    }
   }
-}
+};
 </script>
