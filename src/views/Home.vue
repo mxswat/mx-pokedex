ec652d177162eb3c51ce7725868f0522ac5872e5
Massimo Mx Cacciapaglia <massimocacciapaglia.13@gmail.com>
2020-08-25 11:56:03 +0200

    Home tes 01

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Welcome to MxPokedex!</h1>
+    <h3>Click on any pokemon or use the search</h3>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
