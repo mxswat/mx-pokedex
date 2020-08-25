0aec0c5cb7f12d37109222831e62dccfc20a4410
Massimo Mx Cacciapaglia <massimocacciapaglia.13@gmail.com>
2020-08-25 11:21:11 +0200

    examples

--- src/vue-apollo.js
+++ src/vue-apollo.js
@@ -9,7 +9,11 @@ Vue.use(VueApollo)
 const AUTH_TOKEN = 'apollo-token'
 
 // Http endpoint
-const httpEndpoint = process.env.VUE_APP_GRAPHQL_HTTP || 'http://mazipan-gql-pokeapi.herokuapp.com/graphql'
+const httpEndpoint = process.env.VUE_APP_GRAPHQL_HTTP || 'http://localhost:4000/graphql'
+// Files URL root
+export const filesRoot = process.env.VUE_APP_FILES_ROOT || httpEndpoint.substr(0, httpEndpoint.indexOf('/graphql'))
+
+Vue.prototype.$filesRoot = filesRoot
 
 // Config
 const defaultOptions = {
@@ -17,7 +21,7 @@ const defaultOptions = {
   httpEndpoint,
   // You can use `wss` for secure connection (recommended in production)
   // Use `null` to disable subscriptions
-  wsEndpoint: process.env.VUE_APP_GRAPHQL_WS || 'ws://mazipan-gql-pokeapi.herokuapp.com/graphql',
+  wsEndpoint: process.env.VUE_APP_GRAPHQL_WS || 'ws://localhost:4000/graphql',
   // LocalStorage token
   tokenName: AUTH_TOKEN,
   // Enable Automatic Query persisting with Apollo Engine
